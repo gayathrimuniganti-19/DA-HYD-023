d878597dbb7a09f6e65721e334db7a63b1ffc260
gayathrimuniganti-19 <gayathrimuniganti19@gmail.com>
2026-07-28 14:48:54 +0530

    Tokens - Day1

---
+++ Day-1.py
@@ -0,0 +1,43 @@
+#print('hello codegnan')
+
+'''
+name = 'Gayathri'
+print(name)
+
+email_id ='gayathribhavani954@gmail.com'
+print(email_id)
+#print(Email_id) #Python is case sensitive --> name error
+#@ans=14
+#print(@ans)
+#@defrrr = 3.4 #no special charaters
+
+#True = 45 #no keywords
+
+batch = 'da23'
+print(batch)
+
+#variable --> named memory location where we store the data, its a placehloder...
+batch = 'cse'
+print(batch)
+
+batch = 23
+print(batch)
+
+#literals --> these are constants (value(s)) -->num,char
+
+#operators --> they perform specific operations --> 7 types
+
+length = 5
+breadth = 3
+area = length * breadth
+print(area)
+'''
+
+#Multiassignment of variables
+name = 'gayathri'
+print(name)
+
+name, location, age = 'gayathri', 'hyd', '22'
+print(name)
+print(location)
+print(age)
